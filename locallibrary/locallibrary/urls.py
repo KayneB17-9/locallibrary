76daf434c233bddaa32e8bbc2aede86edf71a5d8
Kayne Barbieri <Kayne@webdev04.local>
2026-04-06 13:08:48 -0400

    Created Skeleton framework for LocalLibrary

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -15,17 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('catalog/', include('catalog.urls'))
 ]
 
-from django.urls import include
-
-urlpatterns = [
-    path('catalog/', include('catalog.urls')),
-]
 
 
 from django.views.generic import RedirectView
